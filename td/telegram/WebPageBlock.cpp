168db7fe912a808197b178c03179ac022d39335a
levlam <levlam@telegram.org>
2026-06-02 19:19:14 +0300

    Use skip_bot_commands in get_rich_message_object.
    (cherry picked from commit e3e63092fe6a202f26875fc6cf24fe6360fda6ca)

--- td/telegram/WebPageBlock.cpp
+++ td/telegram/WebPageBlock.cpp
@@ -63,6 +63,7 @@ struct GetWebPageBlockObjectContext {
   Slice base_url_;
   string real_url_host_;
   string real_url_rhash_;
+  bool skip_bot_commands_ = false;
 
   bool is_first_pass_ = true;
   bool has_anchor_urls_ = false;
@@ -368,6 +369,9 @@ class RichText {
         return td_api::make_object<td_api::richTextCashtag>(texts[0].get_rich_text_object(context),
                                                             trim_first(texts[0].get_full_text(), '$'));
       case RichText::Type::BotCommand:
+        if (context->skip_bot_commands_) {
+          return texts[0].get_rich_text_object(context);
+        }
         return td_api::make_object<td_api::richTextBotCommand>(texts[0].get_rich_text_object(context),
                                                                trim_first(texts[0].get_full_text(), '/'));
       case RichText::Type::AutoUrl:
@@ -3913,7 +3917,8 @@ vector<unique_ptr<WebPageBlock>> clone_web_page_blocks(const vector<unique_ptr<W
 }
 
 vector<td_api::object_ptr<td_api::PageBlock>> get_page_blocks_object(
-    const vector<unique_ptr<WebPageBlock>> &page_blocks, Td *td, Slice base_url, Slice real_url) {
+    const vector<unique_ptr<WebPageBlock>> &page_blocks, Td *td, Slice base_url, Slice real_url,
+    bool skip_bot_commands) {
   GetWebPageBlockObjectContext context;
   context.td_ = td;
   context.base_url_ = base_url;
@@ -3924,6 +3929,7 @@ vector<td_api::object_ptr<td_api::PageBlock>> get_page_blocks_object(
       context.real_url_rhash_ = string();
     }
   }
+  context.skip_bot_commands_ = skip_bot_commands;
   auto blocks = get_page_blocks_object(page_blocks, &context);
   if (!context.has_anchor_urls_) {
     return blocks;
