b4d55dc4ee1d6a6e48b07131a119055f9cc7adcd
levlam <levlam@telegram.org>
2026-06-10 15:23:33 +0300

    Make pageBlockCaption.credit nullable.
    (cherry picked from commit 39d1662960f105e4632dc452267b16dc9c53dac5)

--- td/telegram/WebPageBlock.cpp
+++ td/telegram/WebPageBlock.cpp
@@ -257,9 +257,13 @@ class RichText {
     }
   }
 
-  td_api::object_ptr<td_api::RichText> get_rich_text_object(GetWebPageBlockObjectContext *context) const {
+  td_api::object_ptr<td_api::RichText> get_rich_text_object(GetWebPageBlockObjectContext *context,
+                                                            bool allow_null = false) const {
     switch (type) {
       case RichText::Type::Plain:
+        if (allow_null && content.empty()) {
+          return nullptr;
+        }
         return td_api::make_object<td_api::richTextPlain>(content);
       case RichText::Type::Bold:
         return td_api::make_object<td_api::richTextBold>(texts[0].get_rich_text_object(context));
@@ -465,7 +469,7 @@ class WebPageBlockCaption {
       return nullptr;
     }
     return td_api::make_object<td_api::pageBlockCaption>(text.get_rich_text_object(context),
-                                                         credit.get_rich_text_object(context));
+                                                         credit.get_rich_text_object(context, true));
   }
 
   template <class StorerT>
